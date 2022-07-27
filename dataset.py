a79500bfac9e4b9b7f3ef3e960fa4e1274e63437
praveen <prvns1997@gmail.com>
2022-07-26 21:11:58 -0400

    used inbuilt trainer

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ class AirlineDataset(Dataset):
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['input_ids'] = torch.tensor(self.encodings['input_ids'][idx])
         item['labels'] = torch.tensor(self.labels[idx])
         return item
 
--- train.py
+++ train.py
@@ -25,34 +25,59 @@ model_name = "distilbert-base-uncased"  # base model
 tokenizer = DistilBertTokenizerFast.from_pretrained(model_name)
 train_texts = np.array_str(train_texts)
 test_texts = np.array_str(test_texts)
+# print(test_texts)
 train_encodings = tokenizer(train_texts, truncation=True, padding=True)
 test_encodings = tokenizer(test_texts, truncation=True, padding=True)
 
 train_dataset = AirlineDataset(train_encodings, train_labels)
 test_dataset = AirlineDataset(test_encodings, test_labels)
 
-model = DistilBertForSequenceClassification.from_pretrained(model_name)
-model.to(device)
-model.train()
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
 
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-optim = AdamW(model.parameters(), lr=5e-5)
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=3,              # total number of training epochs
+    per_device_train_batch_size=8,  # batch size per device during training
+    per_device_eval_batch_size=16,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
 
-num_epoch = 5
-for epoch in range(num_epoch):
-    for batch in train_loader:
-        optim.zero_grad()
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
 
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset       # training dataset            # evaluation dataset
+)
 
-        loss = outputs[0]
-        loss.backward()
-        optim.step()
+trainer.train()
 
-model.eval()
 
+# model = DistilBertForSequenceClassification.from_pretrained(model_name)
+# model.to(device)
+# model.train()
+#
+# train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+# optim = AdamW(model.parameters(), lr=5e-5)
+#
+# num_epoch = 5
+# for epoch in range(num_epoch):
+#     for batch in train_loader:
+#         optim.zero_grad()
+#         input_ids = batch['input_ids'].to(device)
+#         attention_mask = batch['attention_mask'].to(device)
+#         labels = batch['labels'].to(device)
+#
+#         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+#
+#         loss = outputs[0]
+#         loss.backward()
+#         optim.step()
+#
+# model.eval()
+#
 result = model.predict(test_dataset)
 print(result)
